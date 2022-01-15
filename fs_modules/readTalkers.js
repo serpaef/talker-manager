a3257ea151736d39b53731eb03929f56abf9967b
Fernando Serpa <serpaef@outlook.com>
2022-01-15 16:18:48 -0300

    updating path of talker.json in module readTalkers

--- fs_modules/readTalkers.js
+++ fs_modules/readTalkers.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises;
 
 async function readTalkers() {
-  return JSON.parse(await fs.readFile('../talker.json'));
+  return JSON.parse(await fs.readFile('talker.json'));
 }
 
 module.exports = readTalkers;
