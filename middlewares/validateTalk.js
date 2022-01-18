f1fd5da94cb653c5c4c2b886f3d2197bc37ebd7d
Fernando Serpa <serpaef@outlook.com>
2022-01-18 11:12:23 -0300

    refact: solves inconsistency at conditions

--- middlewares/validateTalk.js
+++ middlewares/validateTalk.js
@@ -3,7 +3,9 @@ const send400 = require('../utils/send400');
 function validateTalk(req, res, next) {
   const { talk } = req.body;
 
-  if (!talk || !talk.watchedAt || !talk.rate) {
+  // correção feita com ajuda do meu amigo Adriano
+  // js, por não ser tipada, pode ser inconsistente com o parâmetro ! quando se trata de numeros
+  if (!talk || talk === {} || talk.watchedAt === undefined || talk.rate === undefined) {
     return send400('O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios', res);
   }
   
